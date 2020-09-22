b5300f943f934a4f641602c9705abd5c21fd0ba8
mbeitler <mbeitler@hamilton.edu>
2020-09-22 14:59:31 -0400

    Getting confused, made some progress

--- Activity_4/Activity4.R
+++ Activity_4/Activity4.R
@@ -55,9 +55,67 @@ abline(dam.mod, lwd=2)
 #Read phenology data
 pheno <- read.csv("/Users/madelynbeitler/GitHub/ENVSTDATA/Activity4Data/a04/red_maple_pheno.csv")
 
+#set up panel of plots with one row and two columns
+par(mfrow=c(1,2))
+plot(pheno$Tmax,pheno$doy, 
+     pch = 20, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Maximum temperature (C)")
 
+plot(pheno$Prcp,pheno$doy, 
+     pch = 20, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Precipitation (mm)")
 
+#### Question 3 ####
+par(mfrow=c(1,2))
+plot(pheno$Lat,pheno$doy, 
+     pch = 20, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Latitude)")
 
+plot(pheno$elev,pheno$doy, 
+     pch = 20, 
+     col = "mediumaquamarine",
+     ylab = "Day of leaf out",
+     xlab =  "Elevation")
+
+par(mfrow=c(1,2))
+plot(pheno$Tmax,pheno$doy, 
+     pch = 20, 
+     col = "snow4",
+     ylab = "Day of leaf out",
+     xlab =  "Max temperature")
+
+pheno$siteDesc <- as.factor(pheno$siteDesc)
+plot(pheno$siteDesc,pheno$doy, 
+     pch = 20, 
+     col = "yellow",
+     ylab = "Day of leaf out",
+     xlab =  "Urban or Rural")
+
+#to stop using the par argument
+dev.off()
+
+#Set up covariance plots
+plot( ~  pheno$Lat + pheno$Tmax+ pheno$Tmin +pheno$Prcp + pheno$elev + pheno$siteDesc)
+
+#Set up the regression
+pheno$urID <- ifelse(pheno$siteDesc == "Urban",1,0)
+pheno$urID
+
+mlr <- lm(pheno$doy ~  pheno$Tmax  + pheno$Prcp + pheno$elev + pheno$urID)
+mlr
+
+#Calculate fitted values from the regression line for each observation.
+mlFitted <- fitted(mlr)
+mlFitted
+
+#Print summary 
+summary(mlr)
 
 
 
